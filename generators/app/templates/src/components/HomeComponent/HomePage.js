d791a4985c2309d2b3633ac6d06719b47fc88d87
Chaitanya <WG45@global.tesco.org>
2016-08-29 09:08:44 +0530

    Updated React Home page

--- generators/app/templates/src/components/HomeComponent/HomePage.js
+++ generators/app/templates/src/components/HomeComponent/HomePage.js
@@ -7,11 +7,15 @@ var HomePage = React.createClass({
     render: function() {
         return(
             <div className="container">
-                <div className="jumbotron">
-                    <h1>Scaffolding of React Flux app</h1>
-                    <p className="well">
-
-                    </p>
+                <div className="jumbotron jumbo">
+                    <img src="../../images/react-logo.png" alt="React Logo" style={{height:"100px"}}/>
+                    <h1 className="heading">React-Flux web application</h1>
+                    <div className="well" style={{backgroundColor:"#ffffff"}}>
+                        <img src="../../images/folderstr.png" />
+                    </div>
+                    <footer>
+                        with <span className="glyphicon glyphicon-heart"></span> from CKR.
+                    </footer>
                 </div>
             </div>
         )
